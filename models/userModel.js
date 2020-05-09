b5c721d763838cc7dc383953f78daba0df6f03d5
Kingsley <kingsleyumujeyan@gmail.com>
2020-05-09 21:57:00 +0100

    UserModel created with roles

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: [
+        {
+            type: String,
+            default: "student",
+            enum: ["student","tutor","admin"]
+        }
+    ],
+    accessToken: {
+        type: String
+    }
+});
+
+const User = mongoose.model('user',UserSchema);
+module.exports = User;
