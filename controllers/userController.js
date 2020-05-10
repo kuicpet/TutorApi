f7f10b35bd52b989645e99ce82f7c9e413d3d69f
Kingsley <kingsleyumujeyan@gmail.com>
2020-05-10 19:27:32 +0100

    Updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,3 +70,64 @@ exports.signin = async (req, res, next) => {
         next(error);
     }
 }
+// Update a User
+exports.updateUser = async (req,res,next) => {
+    try {
+        const update = req.body;
+        const userId = req.params.userId;
+        await User.findByIdAndUpdate(userId,update);
+        const user = await User.findById(userId);
+        res.status(200).json({
+            data: user,
+            message: "User has been Updated!"
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+// Delete a User
+exports.deleteuser = async (req,res,next) => {
+    try {
+        const userId = req.params.userId;
+        await User.findByIdAndDelete(userId);
+        res.status(200).json({
+            data: null,
+            message: "User has been Deleted!"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+// Access Control for Users
+exports.grantAcces = function(action, resource){
+    return async (req,res,next) => {
+        try {
+            const permission = roles.can(req.user.role)[action](resource);
+            if(!permission.granted){
+                return res.status(401).json({
+                    error: "You don't have enough permission to perform this action"
+                });
+            }
+            next()
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+//Access for Logged In Users
+exports.allowIfLoggedIn = async (req,res,next) => {
+    try {
+        const user = res.locals.loggedInUser;
+        if(!user)
+        return res.status(401).json({
+            error: "You need to be logged in to access this route"
+        });
+        req.user = user;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
