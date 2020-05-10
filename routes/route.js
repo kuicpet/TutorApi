9f51a7872c11cac667d7be25999e802e90674504
Kingsley <kingsleyumujeyan@gmail.com>
2020-05-10 19:47:25 +0100

    Updated

--- routes/route.js
+++ routes/route.js
@@ -5,3 +5,10 @@ const userController = require('../controllers/userController');
 
 router.post('/api/v1/signup',userController.signup);
 router.post('/api/v1/signin',userController.signin);
+router.get('/api/v1/user/:userId',userController.allowIfLoggedIn,userController.getUser);
+router.get('/api/v1/users',userController.allowIfLoggedIn,userController.grantAcces("readAny","subject"),userController.getUsers);
+router.put('/api/v1/user/:userId',userController.allowIfLoggedIn,userController.grantAcces("updateAny","subject"),userController.updateUser);
+router.delete('/api/v1/user/:userId',userController.allowIfLoggedIn,userController.grantAcces("deleteAny","subject"),userController.deleteuser);
+
+
+module.exports = router;
